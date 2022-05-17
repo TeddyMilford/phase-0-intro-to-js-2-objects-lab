a808c925e3abe6a7d41a52a12e31fd3a851923a9
Teddy Milford <teddy.milford@gmail.com>
2022-05-17 09:57:42 -0600

    commit tree 38af84cec1f8849951a12e7086b48d0ba07cf589

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Teddy",
+  streetAddress: "123 Apple street",
+};
+
+const updateEmployeeWithKeyAndValue = function (employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+
+  return newEmployee;
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = function (
+  employee,
+  key,
+  value
+) {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = function (employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+};
+
+const destructivelyDeleteFromEmployeeByKey = function (employee, key) {
+  delete employee[key];
+  return employee;
+};
